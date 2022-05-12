a19987f8bcd761eaf5c7ea48a609b838cd0d0fc5
imry <imry.sadan@zeekit.me>
2022-05-12 07:54:32 +0300

    Add ssh script

---
+++ ssl-tls.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail -o errtrace
+IFS=$'\n\t'
+
+#Create two files with crt and private key
+# Name of the files is the same as the cn
+filename=$1
+
+openssl req -x509 -sha256 -nodes -days 5 -newkey rsa:4096 -subj '/O=www./CN='$filename -keyout $filename.key -out $filename.crt
